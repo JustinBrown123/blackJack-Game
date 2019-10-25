9fa19982b70106319696ca311b64db6cbef8c3bf
Justin Brown <j-brown123@live.com>
2019-10-25 13:34:31 -0400

    added playing card deck generator

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,37 @@
-export default () => {
-    alert("it works!")
-  }
+import Blackjack from "blackjack-dealer-logic"
+
+   export default () =>{
+
+    const game = blackjack.singleDeckGame()
+    const gamesIsRunning = true;
+    const playButton = document.getElementById("playbbutton")
+   
+    playButton.onclick = function(){
+        while (gamesIsRunning){
+        game.deal()
+
+        game.resetPlayers();
+        }
+    }
+
+
+
+
+    hitButton.onclick = function(){
+        singleDeckGame.hitUser();
+        singleDeckGame.evaluateUser();
+    }
+
+    standButton.onclick = function(){
+        singleDeckGame.standUser();
+        singleDeckGame.evaluateUser();
+    }
+
+    doubleButton.onclick = function(){
+        singleDeckGame.doubleUser();
+        singleDeckGame.evaluateUser();
+    }
+
+    const blackjack = new Blackjack
+}
+
